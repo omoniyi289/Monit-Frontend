54e86c02df9025e50debcea03812e086ca455cdc
Innocent Onyemaenu <[innocent.o@e360africa.com]>
2018-04-15 02:03:59 +0100

    done

--- ci-cd.sh
+++ ci-cd.sh
@@ -1,23 +1,18 @@
-#!/bin/bash
 
-##### CI #####
-#composer install
-#php artisan key:generate --env=testing
-#php artisan migrate --force
-#./vendor/bin/phpunit #No tests yet
-
-#### CD ####
-#rsync -vzrh --exclude-from="deploy_exclude.txt" . root@185.130.207.215:/var/www/SMfrontend-Bundle/temp
+rsync -vzrh --exclude-from="deploy_exclude.txt" . root@185.130.207.215:/var/www/SMfrontend-Bundle/temp
 ssh root@185.130.207.215 <<-EOF
-
+    
     cd /var/www/SMfrontend-Bundle
+    mv live/node_modules/ node-mods-bk  #backup node modules
     rm -rf ./backup # Delete previous backup
     mv ./live ./backup # Create new backup
     mv ./temp ./live
+    mv node-mods-bk live/node_modules/ # move node modules back
     mkdir ./temp # create new temp directory for next deployment
     cd ./live
+    export PATH=/var/lib/jenkins/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation/recent_node/bin/
+    
     npm install
-    npm update
     npm run dev
     chmod -R 755 .
     chown -R www-data:www-data .
