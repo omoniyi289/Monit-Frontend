c7c26244ad6ffe0bf8b85b204c09ea35db415614
Innocent Onyemaenu <[innocent.o@e360africa.com]>
2018-05-04 17:08:29 +0100

    paginated data on dashboard

--- src/components/pages/tank/manage.vue
+++ src/components/pages/tank/manage.vue
@@ -190,8 +190,11 @@
 </template>
 <script>
     import Vue from 'vue'; import store from 'src/store/store.js';
-    import datatable from "components/plugins/DataTable/DataTable.vue";import csview from "components/plugins/Company-Station-View/CSView.vue";
-    import VueForm from "vue-form";     import vueSmoothScroll from 'vue-smoothscroll';     Vue.use(vueSmoothScroll);
+    import datatable from "components/plugins/DataTable/DataTable.vue";
+    import csview from "components/plugins/Company-Station-View/CSView.vue";
+    import VueForm from "vue-form";     
+    import vueSmoothScroll from 'vue-smoothscroll';     
+    Vue.use(vueSmoothScroll);
     import options from "src/validations/validations.js";
     Vue.use(VueForm, options);
     export default {
